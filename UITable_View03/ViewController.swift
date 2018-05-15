a663645fb1446c62e34077759996f9c39141e42f
DIT <d7703_21@d7703-21ui-imac.local>
2018-05-15 15:20:29 +0900

    first

--- UITable_View03/ViewController.swift
+++ UITable_View03/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mySlide: UILabel!
+    @IBOutlet weak var myslider: UISlider!
+    @IBOutlet weak var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func slide(_ sender: Any) {
+        let val = Int(myslider.value)
+        print(val)
+        mySlide.text = String(val)
+        
+    }
+    
 
 }
 

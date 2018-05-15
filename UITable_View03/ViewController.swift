d8008b772d1452e233b5f65998c56c61f2030a01
DIT <d7703_21@d7703-21ui-imac.local>
2018-05-15 14:17:44 +0900

    Initial Commit

---
+++ UITable_View03/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UITable_View03
+//
+//  Created by D7703_21 on 2018. 5. 15..
+//  Copyright © 2018년 D7703_21. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
